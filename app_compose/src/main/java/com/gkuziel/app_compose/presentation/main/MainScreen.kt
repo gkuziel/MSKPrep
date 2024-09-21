4a0d087d44fb597477a4f80e52fe4238bdaf1910
DESKTOP-QGILQOA\grzeg <grzegorz.kuziel@gmail.com>
2024-09-21 17:31:10 +0100

    issue fixed

--- app_compose/src/main/java/com/gkuziel/app_compose/presentation/main/MainScreen.kt
+++ app_compose/src/main/java/com/gkuziel/app_compose/presentation/main/MainScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -30,7 +31,9 @@ fun MainScreen(
 ) {
     val uiSate = viewModel.events.collectAsState()
 
-    viewModel.loadUsers()
+    LaunchedEffect(Unit) {
+        viewModel.loadUsers()
+    }
 
     Column(
         modifier = Modifier
